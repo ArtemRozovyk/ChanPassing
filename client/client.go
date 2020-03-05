f3a57f286d49aab6d35f6dc8f6c2c568c560b64b
RozovykArtemiy <rozovyk.artem@gmail.com>
2020-03-05 18:30:54 +0100

    1v

--- client/client.go
+++ client/client.go
@@ -22,16 +22,22 @@ var NB_G int = 2                                           // nombre de gestionn
 var NB_P int = 2                                           // nombre de producteurs
 var NB_O int = 4                                           // nombre d'ouvriers
 var NB_PD int = 2                                          // nombre de producteurs distants pour la Partie 2
-var lnnb int =2;
+var lnnb int = 2
 var pers_vide = st.Personne{Nom: "", Prenom: "", Age: 0, Sexe: "M"} // une personne vide
 
 // paquet de personne, sur lequel on peut travailler, implemente l'interface personne_int
 
-type personne_emp struct  {
-	pers st.Personne
-	ligne int
-	afaire []func(st.Personne) st.Personne
-	status string
+type personne_emp struct {
+	pers    st.Personne
+	ligne   int
+	lecteur chan message_lec
+	afaire  []func(st.Personne) st.Personne
+	status  string
+}
+
+type message_lec struct {
+	retour chan string
+	ligne  int
 }
 
 // paquet de personne distante, pour la Partie 2, implemente l'interface personne_int
@@ -65,23 +71,23 @@ func personne_de_ligne(l string) st.Personne {
 func (p *personne_emp) initialise() {
 	// A FAIRE
 	recupLine := make(chan string)
-	go func(rL chan string){lecteur(rL,p.ligne) }(recupLine)
-	line :=<- recupLine
-	p.pers=personne_de_ligne(line)
-	p.status="R"
+	p.lecteur <- message_lec{retour: recupLine, ligne: p.ligne}
+	line := <-recupLine
+	p.pers = personne_de_ligne(line)
+	p.status = "R"
 	for i := 0; i < 5; i++ {
-		p.afaire=append(p.afaire,tr.UnTravail())
+		p.afaire = append(p.afaire, tr.UnTravail())
 	}
 
 }
 
 func (p *personne_emp) travaille() {
 	var tache func(st.Personne) st.Personne
-	tache,p.afaire = p.afaire[0],p.afaire[1:]
+	tache, p.afaire = p.afaire[0], p.afaire[1:]
 	tache(p.pers)
-	if len(p.afaire)==0 {
-		p.status="C"
-	}else{
+	if len(p.afaire) == 0 {
+		p.status = "C"
+	} else {
 		print(len(p.afaire))
 		//fmt.Println("Length : ",len(p.afaire), p.pers.Nom,p.pers.Prenom)
 
@@ -89,7 +95,7 @@ func (p *personne_emp) travaille() {
 }
 
 func (p *personne_emp) vers_string() string {
-	return p.pers.Nom +" "+p.pers.Prenom+ " " + p.status
+	return p.pers.Nom + " " + p.pers.Prenom + " " + p.status
 	//TODO
 }
 
@@ -127,23 +133,31 @@ func proxy() {
 }
 
 // Partie 1 : contacté par la méthode initialise() de personne_emp, récupère une ligne donnée dans le fichier source
-func lecteur(recupLine chan string, lNb int) {
-	i:=0
-	file, err := os.Open("./client/conseillers-municipaux.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line :=scanner.Text()
-		if i ==lNb {
-			recupLine<-line
-		}
-		i++
-	}
+func lecteur(recupLine chan message_lec) {
+	for {
+		lNb := <-recupLine
+		fmt.Println("Lecteur solicité pour la ligne ", lNb.ligne)
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		file, err := os.Open("./client/1-rne-cm.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		scanner := bufio.NewScanner(file)
+		for i := 0; i < lNb.ligne; i++ {
+			_ = scanner.Scan()
+		}
+
+		line := scanner.Scan()
+		if line == false {
+			log.Fatal(err)
+		} else {
+			r := scanner.Text()
+			fmt.Print("La ligne trouvé:  ", r)
+			lNb.retour <- r
+		}
+
+		file.Close()
 	}
 
 }
@@ -153,15 +167,15 @@ func lecteur(recupLine chan string, lNb int) {
 // Si le statut est R, ils travaille une fois sur le paquet puis le repasse aux gestionnaires
 // Si le statut est C, ils passent le paquet au collecteur
 
-
 // Partie 1: les producteurs cree des personne_int implementees par des personne_emp initialement vides,
 // de statut V mais contenant un numéro de ligne (pour etre initialisee depuis le fichier texte)
 // la personne est passée aux gestionnaires
-func producteur(gestChan chan personne_int) {
+func producteur(gestChan chan personne_int, lec chan message_lec) {
+	var b = 1
 	for {
-		initPack := personne_emp{pers_vide,lnnb,make([]func(st.Personne) st.Personne, 0),"V"}
-		lnnb++
-		gestChan<- &initPack
+		initPack := personne_emp{pers_vide, b, lec, make([]func(st.Personne) st.Personne, 0), "V"}
+		b++
+		gestChan <- &initPack
 	}
 
 	// A FAIRE
@@ -180,73 +194,74 @@ func producteur_distant() {
 // ATTENTION: la famine des ouvriers doit être évitée: si les producteurs inondent les gestionnaires de paquets, les ouvrier ne pourront
 // plus rendre les paquets surlesquels ils travaillent pour en prendre des autres
 
-var currSent = 0
-
 func gestionnaire(fromProducer chan personne_int, toOuvr chan personne_int, fromOuv chan personne_int, ack chan int) {
-
-	var queue [] personne_int
-	var prs personne_int
-
-	for{
-			select{
-			case pp:= <-fromProducer:
-				if len(queue)<(TAILLE_G/2) {
-					//print("getting from p\n")
-					queue = append(queue, pp)
-				}
-			case po:= <-fromOuv:
-				if len(queue)<(TAILLE_G) {
-					//println("got one from ouvr"+po.vers_string())
-					//queue=append([]personne_int{po},queue...)
-					queue = append(queue, po)
-					currSent--
-				}
-						//fmt.Println("Length : ",len(queue))
-			}
-			if currSent<TAILLE_G{
-				prs,queue= queue[0],queue[1:]
-				go func(p1 personne_int) {toOuvr<-p1}(prs)
-				currSent++
-				println("sent to ouv "+prs.vers_string())
+	//var queue [] personne_int
+	queue := make([]personne_int, 0)
+	for {
+		switch len(queue) {
+		case TAILLE_G:
+			toOuvr <- queue[0]
+			queue = queue[1:]
+		case 0:
+			select {
+			case np := <-fromOuv:
+				queue = append(queue, np)
+			case np := <-fromProducer:
+				queue = append(queue, np)
 			}
 
+		default:
+			if len(queue) < TAILLE_G/2 {
+				select {
+				case np := <-fromOuv:
+					queue = append(queue, np)
+				case np := <-fromProducer:
+					queue = append(queue, np)
+				case toOuvr <- queue[0]:
+					queue = queue[1:]
+				}
+			} else {
+				select {
+				case np := <-fromOuv:
+					queue = append(queue, np)
+				case toOuvr <- queue[0]:
+					queue = queue[1:]
+				}
+			}
+		}
 	}
 }
 
-func ouvrier(fromGest chan personne_int, togest chan personne_int, collect chan personne_int,ack chan int ) {
-	for{
-		pers :=<-fromGest
+func ouvrier(fromGest chan personne_int, togest chan personne_int, collect chan personne_int, ack chan int) {
+	for {
+		pers := <-fromGest
 		switch pers.donne_statut() {
 		case "V":
 			println("got V from gest")
 			pers.initialise()
-			togest<-pers
+			println("sending to gest " + pers.vers_string())
+			togest <- pers
 			//ack<-0
-			println("sent to gest "+pers.vers_string() )
+			println("sent to gest " + pers.vers_string())
 		case "R":
 			println("got R from gest")
 			pers.travaille()
-			togest<-pers
-			println("sent R to gest")
-		case "C" :
-			collect<-pers
-
+			togest <- pers
+			println("sent R to gest " + pers.vers_string())
+		case "C":
+			println("sent to collect")
+			collect <- pers
 		}
 	}
-
 	// A FAIRE
 }
-
-
 
 // Partie 1: le collecteur recoit des personne_int dont le statut est c, il les collecte dans un journal
 // quand il recoit un signal de fin du temps, il imprime son journal.
 func collecteur(fromOuvr chan personne_int) {
-	for{
-		println("DONE "+(<-fromOuvr).vers_string())
-		currSent--
+	for {
+		println("DONE " + (<-fromOuvr).vers_string())
 	}
-
 	// A FAIRE
 }
 
@@ -256,26 +271,21 @@ func main() {
 		fmt.Println("Format: client <port> <millisecondes d'attente>")
 		return
 	}
-
 	//port, _ := strconv.Atoi(os.Args[1]) // utile pour la partie 2
-	millis, _ := strconv.Atoi(os.Args[2]) // duree du timeout 
+	millis, _ := strconv.Atoi(os.Args[2]) // duree du timeout
 	fintemps := make(chan int)
-	prodGest :=make(chan personne_int)
-	gestOuvr :=make(chan personne_int)
-	gestOuvr2 :=make(chan personne_int)
+	prodGest := make(chan personne_int)
+	gestOuvr := make(chan personne_int)
+	gestOuvr2 := make(chan personne_int)
 	ouvrCollect := make(chan personne_int)
+	toLec := make(chan message_lec)
 	ack := make(chan int)
-	go func() {producteur(prodGest)}()
-	go func() {gestionnaire(prodGest,gestOuvr,gestOuvr2,ack)}()
-	go func() {ouvrier(gestOuvr,gestOuvr2,ouvrCollect,ack)}()
+	go func() { producteur(prodGest, toLec) }()
+	go func() { gestionnaire(prodGest, gestOuvr, gestOuvr2, ack) }()
+	go func() { ouvrier(gestOuvr, gestOuvr2, ouvrCollect, ack) }()
 	//go func() {producteur(ouvrCollect)}()
-	go func() {collecteur(ouvrCollect)}()
-
-
-
-
-
-
+	go func() { collecteur(ouvrCollect) }()
+	go func() { lecteur(toLec) }()
 	// A FAIRE
 	// creer les canaux
 	// lancer les goroutines (parties 1 et 2): 1 lecteur, 1 collecteur, des producteurs, des gestionnaires, des ouvriers
